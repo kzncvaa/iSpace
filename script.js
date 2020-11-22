c0d7b56665562d0453b79652bcf48372ef7a120e
Arina <ieroarina@gmail.com>
2020-11-22 16:44:14 +0200

    new

--- script.js
+++ script.js
@@ -55,4 +55,12 @@ $(window).scroll(function(){
         });
     }
 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+    if(isSafari){
+        $('.container').css({zoom : '110%'});
+    }
+
+
 });
+
