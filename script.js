30134c869e74e42516b58746b4569e9d90fd7649
Arina <ieroarina@gmail.com>
2020-11-22 11:14:37 +0200

    new

--- script.js
+++ script.js
@@ -27,57 +27,9 @@ $(document).ready(function () {
     if(document.documentElement.scroll>1100){
         $('.wrapper__aside').css({display : `none`});
     }
-});
 
-// var anchors = [];
-// var currentAnchor = -1;
-// var isAnimating  = false;
-// $(function(){
-//     function updateAnchors() {
-//         anchors = [];
-//         $('.anchor').each(function(i, element){
-//             anchors.push( $(element).offset().top );
-//         });
-//     }
-//     $('body').on('mousewheel', function(e){
-//         e.preventDefault();
-//         e.stopPropagation();
-//         if( isAnimating ) {
-//             return false;
-//         }
-//         isAnimating  = true;
-//         if( e.originalEvent.wheelDelta >= 0 ) {
-//             currentAnchor--;
-//         }else{
-//             currentAnchor++;
-//         }
-//         if( currentAnchor > (anchors.length - 1) 
-//            || currentAnchor < 0 ) {
-//             currentAnchor = 0;
-//         }
-//         isAnimating  = true;
-//         $('html, body').animate({
-//             scrollTop: parseInt( anchors[currentAnchor] )
-//         }, 300, 'swing', function(){
-//             isAnimating  = false;
-//         });
-//     });
-//     updateAnchors();   
-// });
 
 $(window).scroll(function(){
-   /* if ($(this).scrollTop() > 5) {
-        $('.services__background').css({transform : `translateX(0)`});
-    };
-    if ($(this).scrollTop() > 400) {
-        $('.choise__background').css({transform : `translateX(0)`});
-    };
-    if ($(this).scrollTop() > 1100) {
-        $('.numbers__background').css({transform : `translateX(0)`});
-    };
-    if ($(this).scrollTop() > 1500) {
-        $('.contacts__background').css({transform : `translateX(0)`});
-    };*/
     if ($(window).scrollTop() > 3000) {
         $('.wrapper__aside').css({display : `none`});
     }
@@ -86,3 +38,18 @@ $(window).scroll(function(){
     };
 });
 
+
+    if($(window).innerWidth() <= 480){
+        $(document).scroll(function () {
+            var y = $(this).scrollTop();
+            var x = $(".jumbotron__content").position();
+            if (y > x.top) {
+                $('.aboutus__nav').fadeIn().css({"position":"fixed", "background" : "#fff", "z-index" : "200"});
+                
+            } else {
+                $('.aboutus__nav').css({"position" : "static", "background" : "none"});
+            }
+        });
+    }
+
+});
